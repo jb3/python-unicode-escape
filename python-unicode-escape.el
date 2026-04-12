f5b27df3240352056aefe8e0380bbd77e51966fe
Joe Banks <joe@jb3.dev>
2026-04-12 21:59:52 +0100

    chore: add SPDX License & license header

--- python-unicode-escape.el
+++ python-unicode-escape.el
@@ -7,6 +7,22 @@
 ;; Homepage: https://github.com/jb3/python-unicode-escape
 ;; URL: https://github.com/jb3/python-unicode-escape
 
+;; SPDX-License-Identifier: GPL-3.0
+
+;; Copyright (C) 2026 Joe Banks
+;;
+;; This program is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, version 3.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU General Public License along with
+;; this program. If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;;
 ;; Adds completion-at-point (CAPF) support for Python's \N{UNICODE NAME}
